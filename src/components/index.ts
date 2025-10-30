93f86280a78527f3598856d876792e011925995a
HamSungJun <tjdwns5123@gmail.com>
2025-10-31 01:22:12 +0900

    feat: export Switch component and update hooks index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Modal } from "./Modal/index.client";
 export type { ModalProps } from "./Modal/index.client";
 export type { ModalLayerProps } from "./Modal/ModalLayer.client";
 export type { ModalPanelProps } from "./Modal/ModalPanel.client";
+
+export { default as Switch } from "./Switch/index.client";
+export type { SwitchProps } from "./Switch/index.client";
